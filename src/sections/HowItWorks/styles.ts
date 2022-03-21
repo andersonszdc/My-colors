55d1fb3c969e63c5f378a2a2621223b51ca9a8e8
Anderson Souza <andersonszdc@gmail.com>
2022-03-21 06:41:11 -0300

    style HowItWorks

---
+++ src/sections/HowItWorks/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 8%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4.8rem;
+  text-align: center;
+
+  .title {
+    font-size: var(--fs-800);
+  }
+
+  .subtitle {
+    font-size: var(--fs-500);
+  }
+`;
